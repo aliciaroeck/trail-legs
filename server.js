3d7e639586fca89d37ccb954c393318b66584b70
aliciaroeck16 <aliciamata00@gmail.com>
2020-07-05 16:09:59 -0500

    File structure

---
+++ server.js
@@ -0,0 +1,79 @@
+/* External Modules */
+const express = require("express");
+const bodyparser = require("body-parser");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const db = require("./models");
+require("dotenv").config();
+
+/* Internal Modules */
+const controllers = require("./controllers");
+
+/* Instanced Modules */
+const app = express();
+
+/* Configuration Variables */
+const PORT = process.env.PORT;
+
+/* App Configuration */
+app.set("view engine", "ejs");
+
+/* Middleware */
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    // store session info into db
+    store: new MongoStore({
+      url: process.env.MONGODB_URI
+    }),
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7 * 2, // two weeks
+    },
+  })
+);
+
+app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname+"/images"));
+
+
+const authRequired = function (req, res, next) {
+  if (!req.session.currentUser) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
+/* Routes */
+// root routes
+app.get("/", (req,res) => {
+    db.City.find({}, (err, allCities) => {
+        if(err){
+            console.log(err);
+            return res.send({message: "Internal Server Error"});
+          } else {
+            const context = 
+            {cities: allCities,
+            user: req.session.currentUser};
+            res.render("index", context);
+          }
+    });
+});
+
+// auth route
+app.use("/", controllers.auth);
+
+// city routes
+app.use("/cities", controllers.city);
+
+// trail routes
+app.use("/trails", authRequired, controllers.trail);
+
+/* Binding Server */
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
